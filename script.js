93d3c12d16b0f508f5140518867c02a675a3f985
Paul Smith <44853199+paulsmith1232@users.noreply.github.com>
2022-12-06 11:17:49 -0500

    Update grid creation and add hover effect
    Updated grid creation function to incoming value and apply correct max
    or min value if it passes limits. Hover effect is now applied to grid
    items and a button has been added to UI to allow for grid reset.

--- script.js
+++ script.js
@@ -6,29 +6,57 @@
 */
 
 let x = 1;
-const sketchContainer = document.querySelector('.sketch-container');
+const grid = document.querySelector('.grid-container');
+const resetButton = document.querySelector('.reset-button');
 
 function createGrid(num = 16){
+  let size = 0;
+  const MAX_SIZE = 100;
+  const MIN_SIZE = 1;
+
+  // check for usable number 
+  if (num > MAX_SIZE) size = MAX_SIZE
+  else if (num < MIN_SIZE) size = MIN_SIZE
+  else size = num; 
+
+  console.log(size);
+
+  grid.textContent = '';
   const div = document.createElement('div');
   div.className = "row";
 
-  // sketchContainer.innerHTML +='<div class="row">'
-  // for(let i = 0; i < num; i++){
-  //   div.innerHTML += '<div class="paint-cell"></div>';
-  // }
 
-  for(let i = 0; i < num; i++){
+  for(let i = 0; i < size; i++){
     const rowDiv = document.createElement('div');
     rowDiv.className = "row";
 
-    for(let y = 0; y < num; y++){
+    for(let y = 0; y < size; y++){
       rowDiv.innerHTML += '<div class="paint-cell"></div>';
     }
-    sketchContainer.appendChild(rowDiv);
-  }
-
-
-  
+    grid.appendChild(rowDiv);
+  }  
+  applyPaintEvents();
+  return document.querySelector('.grid-container');
 }
 
-createGrid(100);
+function changeColour(e) {
+  e.target.classList.add('filled');
+}
+
+function applyPaintEvents(){
+  const paintCells = document.querySelectorAll('.paint-cell');
+  for(const cell of paintCells) {
+    cell.addEventListener('mouseover', (e) => changeColour(e));
+  }
+}
+
+resetButton.addEventListener('click', () => createGrid(prompt('Enter a number', 16)));
+
+createGrid();
+// applyPaintEvents();
+
+
+
+
+
+
